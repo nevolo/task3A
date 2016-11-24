701f229ed1abd96c5691d2ec8db8ca89ab8d0085
Aleksandr <kazadoev.aleksandr@mail.ru>
2016-11-25 02:59:14 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -11,497 +11,121 @@ app.get('/', (req, res) => {
   });
 });
 
-// -------------№ 1 --------------
-app.get('/task3A', (req, res) => {
-	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
+
+var pc = {};
+var result = {};
+var count;
+var path;
+
+const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
 
 	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(result);
+	.then(async (response) => {
+		pc = await response.json();
 	  })
 	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
+		console.log('Чтото пошло не так:', err);
 	  });
-});
 
-// -------------№ 2 --------------
-app.get('/task3A/board', (req, res) => {
+app.get(/^(?:task3A\/)?(.*?)(?:\/)?$/, (req, res) => { 
 
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
+	count = 0;
+	path = req.path; 
+	path = path.replace('/task3A/', "");
+	path = path.split('/'); //массив путей
 
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(result.board);
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 3 --------------
-app.get('/task3A/ram', (req, res) => {
-
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(result.ram);
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 4 --------------
-app.get('/task3A/os', (req, res) => {
-
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.os));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 5 --------------
-app.get('/task3A/hdd', (req, res) => {
-
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.hdd));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 6 --------------
-app.get('/task3A/board/vendor', (req, res) => {
-
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.board.vendor));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 7 --------------
-app.get('/task3A/board/model', (req, res) => {
-
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.board.model));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 8 --------------
-app.get('/task3A/board/cpu', (req, res) => { console.log(req.path);
-
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.board.cpu));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 9 --------------
-app.get('/task3A/board/image', (req, res) => { console.log(req.path);
-	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.board.image));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 10 --------------
-app.get('/task3A/board/video', (req, res) => { console.log(req.path);
-	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.board.video));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 11 --------------
-app.get('/task3A/ram/vendor', (req, res) => { console.log(req.path);
-	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.ram.vendor));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 12 --------------
-app.get('/task3A/ram/volume', (req, res) => { console.log(req.path);
-	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.ram.volume));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 13 --------------
-app.get('/task3A/ram/pins', (req, res) => { console.log(req.path);
-	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.ram.pins));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 14 --------------
-app.get('/task3A/volumes', (req, res) => { console.log(req.path);
-	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-		
-		function getVolumes(hdd) {
-			let v = {};
-			for (let key in hdd) {
-				const d = hdd[key].volume; //строка с именем диска
-				if (!v[d])
-					v[d] = 0;
-				v[d] += hdd[key].size;
-			}
-			for (let key in v) {
-				v[key] = v[key] + 'B';
-			}
-			return v;
+	for (var i = 0; i < path.length; i++) {
+		if ((path.length > 1) && (path[i] == '')) {
+			path.splice(i, 1);
 		}
+	}
+	
+	if (path[0] == '') { 
+		res.send(pc);
+	} else if (path[0] == 'volumes') {
+		res.send(getVolumes(pc.hdd));
+	} else {
+	
+		for (var key in pc) {
+			if (path[0] == key) {
+				result = getResult(path.length, pc);
+			} 
+		}
+
+		if (result == 'Not Found') {
+			res.status(404).send(result);	
+		} else {
+			result = JSON.stringify(result);
+			res.send(result);
+		}
+	} 
+});
+
+	function getResult (length, json) { 
+	
+		if (typeof json === 'string') {
+			var tmp = {};
+			tmp[count] = json;
+			json = tmp;
+		}
+	
+		if (Array.isArray(json)) {
+			var tmp = {};
+			for (var i = 0; i < json.length; i++) {
+				tmp[i] = json[i];
+			}
+			
+			json = tmp;
+		}
+	
+		if (length == 1) {
 		
-	    res.send(getVolumes(result.hdd));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 15 --------------
-app.get('/task3A/floppy', (req, res) => { console.log(req.path);
+			if (path[count] in json) { 
+				return json[path[count]];
+			} else return 'Not Found';	
 	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.floppy));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 16 --------------
-app.get('/task3A/hdd/0', (req, res) => { console.log(req.path);
+		} else if (json.hasOwnProperty(path[count])) { 
+			return getResult(--length, json[path[count++]]); 	
+		} else return 'Not Found';		
+	}
 	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
+	function getVolumes (hdd) {
+		var volumeRes = {};
+		
+		for (var key in hdd) {
+			if (!volumeRes[hdd[key].volume]) {
+				volumeRes[hdd[key].volume] = 0;	
+			}
+			if (hdd[key].volume in volumeRes) {
+				volumeRes[hdd[key].volume] += hdd[key].size;
+			}
+		}
 
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.hdd[0]));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 17 --------------
-app.get('/task3A/hdd/0/vendor', (req, res) => { console.log(req.path);
-	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.hdd[0].vendor));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 18 --------------
-app.get('/task3A/hdd/0/size', (req, res) => { console.log(req.path);
-	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.hdd[0].size));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 19 --------------
-app.get('/task3A/hdd/0/volume', (req, res) => { console.log(req.path);
-	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.hdd[0].volume));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 20 --------------
-app.get('/task3A/hdd/1', (req, res) => { console.log(req.path);
-	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.hdd[1]));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 21 --------------
-app.get('/task3A/hdd/1/volume', (req, res) => { console.log(req.path);
-	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.hdd[1].volume));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------№ 22 --------------
-app.get('/task3A/hdd/22', (req, res) => { console.log(req.path);
-	
-	res.status(404).send("Not Found");
-	
-});
-
-// -------------№ 23 --------------
-app.get('/task3A/hdd/222', (req, res) => { console.log(req.path);
-	
-	res.status(404).send("Not Found");
-	
-});
-
-// -------------№ 24 --------------
-app.get('/task3A/other', (req, res) => { console.log(req.path);
-	
-	res.status(404).send("Not Found");
-	
-});
-
-// -------------№ 25 --------------
-app.get('/task3A/some/other', (req, res) => { console.log(req.path);
-	
-	res.status(404).send("Not Found");
-	
-});
-
-// -------------№ 26 --------------
-app.get('/task3A/hdd/some/other', (req, res) => { console.log(req.path);
-	
-	res.status(404).send("Not Found");
-	
-});
-
-// -------------№ 27 --------------
-app.get('/task3A/hdd/-1', (req, res) => { console.log(req.path);
-	
-	res.status(404).send("Not Found");
-	
-});
-
-// -------------- № 28 --------------
-app.get('/task3A/hdd.0', (req, res) => { console.log(req.path);
-	
-	res.status(404).send("Not Found");
-	
-});
-
-// -------------- № 30 --------------
-app.get('/task3A/monitor', (req, res) => { console.log(req.path);
-	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.monitor));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------- № 35 --------------
-app.get('/task3A/length', (req, res) => { console.log(req.path);
-	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.length));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------- № 36 --------------
-app.get('/task3A/height', (req, res) => { console.log(req.path);
-	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.height));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------- № 37 --------------
-app.get('/task3A/width', (req, res) => { console.log(req.path);
-	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.width));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-// -------------- № 39 --------------
-app.get('/task3A/board/cpu/hz', (req, res) => { console.log(req.path);
-	
-	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-	fetch(pcUrl)
-	  .then(async (response) => {
-	    
-	    var result = await response.json();
-	    res.send(JSON.stringify(result.board.cpu.hz));
-	  })
-	  .catch(err => {
-	    console.log('Чтото пошло не так:', err);
-	  });
-});
-
-
-// -------------- № 29 --------------
-app.get('*', (req, res) => { console.log(req.path);
-	
-	res.status(404).send("Not Found");
-	
-});
+		for (var key in volumeRes) { 
+			volumeRes[key] += 'B';
+		}
+		return volumeRes;
+	}
 
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
+
+
+/* app.get(/^(?:task3A\/)?(.*?)(?:\/)?$/, function(req, res) {
+    let path = req.params[0]; */
+	
+/* app.get('task3A/:param', function(req, res) { */
+
+/* app.get('/volumes', (req, res) => some code);
+   app.get('*', (req, res) => some code); */
+   
+   
+/* var jsonLength = Object.keys(result).length; */ //длина json объекта
+
+/* for (var key in pc) {
+console.log(key, pc[key]);
+} */
 
 
 /*{
